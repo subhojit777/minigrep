67aeed6f2cedb584f2bec25b6f0fae2836f0448b
Subhojit Paul <subhojitpaul21@gmail.com>
2018-06-25 11:53:09 +0530

    init - working

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -6,10 +6,10 @@ use minigrep::*;
 pub fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let parameters = minigrep::parse_config(&args);
+    let parameters: Config = minigrep::parse_config(&args);
 
-    let file_content = minigrep::read_file(parameters.1);
+    let file_content: String = minigrep::read_file(parameters.get_filename());
 
-    let has_search_string = minigrep::search(&file_content, parameters.0);
+    let has_search_string: bool = minigrep::search(&file_content, parameters.get_query());
     print!("{}\n", has_search_string);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+pub struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    pub fn new(query: &str, filename: &str) -> Config {
+        Config { query: query.to_string(), filename: filename.to_string() }
+    }
+
+    pub fn get_query(&self) -> &str {
+        &self.query
+    }
+
+    pub fn get_filename(&self) -> &str {
+        &self.filename
+    }
+}
+
 pub fn read_file(filename: &str) -> String {
     let mut f = File::open(filename).expect("File not found");
     let mut file_content = String::new();
@@ -15,9 +34,6 @@ pub fn search(file_content: &str, search_string: &str) -> bool {
     file_content.contains(search_string)
 }
 
-pub fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
+pub fn parse_config(args: &[String]) -> Config {
+    Config::new(&args[1], &args[2])
 }
