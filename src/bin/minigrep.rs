3eb770c1a9e19e6a3ba226c678a58e4b5d4f9eb2
Subhojit Paul <subhojitpaul21@gmail.com>
2018-07-03 11:39:33 +0530

    better search output - WIP

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -1,8 +1,10 @@
 extern crate minigrep;
+extern crate colored;
 
 use std::env;
 use std::process;
 use minigrep::*;
+use colored::*;
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,7 +15,28 @@ pub fn main() {
     });
 
     let file_content: String = minigrep::read_file(config.get_filename()).expect("Something went wrong.");
+    let matched_indices = minigrep::search(&file_content, config.get_query());
 
-    let has_search_string = minigrep::search(&file_content, config.get_query());
-    print!("{:?}\n", has_search_string);
+    // No need to proceed if no match is found.
+    if matched_indices.len() == 0 {
+        process::exit(0);
+    }
+
+    // Print the file content, and highlight the query.
+    let query_length = config.get_query().len();
+    let mut highlight_query_counter = 0;
+    let file_content_char_indices = file_content.char_indices();
+    let matched_indices_as_iter = matched_indices.iter();
+    let mut matched_index = matched_indices_as_iter.next().unwrap();
+
+    for i in file_content_char_indices {
+        if i.0 == *matched_index || highlight_query_counter < query_length {
+            print!("{}", (i.1).green().bold());
+            highlight_query_counter += 1;
+            matched_index = matched_indices_as_iter.next().unwrap();
+        } else {
+            highlight_query_counter = 0;
+            print!("{}", i.1);
+        }
+    }
 }
