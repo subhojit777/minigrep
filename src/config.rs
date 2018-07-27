d36e3a9b2ff29ed3a372df2b630230d5e94a5c68
Subhojit Paul <subhojitpaul21@gmail.com>
2018-07-27 12:11:20 +0530

    Config filename -> File - WIP

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,25 @@
 use options::*;
 use super::{GenError, GenResult};
 use super::minigrep_error::*;
+use std::fs::File;
 
 /// The necessary configurations for initializing minigrep.
 pub struct Config {
     options: Option<Options>,
     query: String,
-    filename: String,
+    file: File,
 }
 
 impl Config {
     /// Initializes a new Config.
     ///
     /// It returns error if incorrect option is passed.
-    pub fn new(options: Option<&str>, query: &str, filename: &str) -> GenResult<Config> {
+    pub fn new(options: Option<&str>, query: &str, file: &File) -> GenResult<Config> {
         if options.is_none() {
             return Ok(Config {
                 options: None,
                 query: query.to_string(),
-                filename: filename.to_string(),
+                file: file.try_clone().unwrap(),
             });
         }
 
@@ -36,7 +37,7 @@ impl Config {
         Ok(Config {
             options: Some(options_struct),
             query: query.to_string(),
-            filename: filename.to_string(),
+            file: file.try_clone().unwrap(),
         })
     }
 
@@ -51,26 +52,25 @@ impl Config {
     }
 
     /// Returns filename for a config.
-    pub fn get_filename(&self) -> &str {
-        &self.filename
+    pub fn get_file(&self) -> &File {
+        &self.file
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::File;
 
     #[test]
     fn test_config_with_options() {
         let query = "query";
-        let filename = "filename";
-
-        let config = Config::new(Some("iw"), query, filename);
+        let file = File::open("./test-data/test.txt").unwrap();
+        let config = Config::new(Some("iw"), query, &file);
 
         assert_eq!(config.is_ok(), true);
 
         let config = config.unwrap();
-
         let options = config.get_options();
 
         assert_eq!(options.is_some(), true);
@@ -80,34 +80,30 @@ mod tests {
         assert_eq!(options.is_case_sensitive(), true);
         assert_eq!(options.is_exact_match(), true);
         assert_eq!(config.get_query(), query);
-        assert_eq!(config.get_filename(), filename);
+        assert_eq!(config.get_file().metadata().unwrap().len(), file.metadata().unwrap().len());
     }
 
     #[test]
     fn test_config_without_options() {
         let query = "query";
-        let filename = "filename";
-
-        let config = Config::new(None, query, filename);
+        let file = File::open("./test-data/test.txt").unwrap();
+        let config = Config::new(None, query, &file);
 
         assert_eq!(config.is_ok(), true);
 
         let config = config.unwrap();
-
         let options = config.get_options();
 
         assert_eq!(options.is_none(), true);
-
         assert_eq!(config.get_query(), query);
-        assert_eq!(config.get_filename(), filename);
+        assert_eq!(config.get_file().metadata().unwrap().len(), file.metadata().unwrap().len());
     }
 
     #[test]
     fn test_config_with_invalid_options() {
         let query = "query";
-        let filename = "filename";
-
-        let config = Config::new(Some("vi"), query, filename);
+        let file = File::open("./test-data/test.txt").unwrap();
+        let config = Config::new(Some("vi"), query, &file);
 
         assert_eq!(config.is_err(), true);
     }
@@ -115,38 +111,41 @@ mod tests {
     #[test]
     fn test_config_get_options() {
         let query = String::from("query");
-        let filename = String::from("filename");
-
+        let file = File::open("./test-data/test.txt").unwrap();
         let config = Config {
             options: Some(Options::new(true, false)),
             query: query,
-            filename: filename,
+            file: file,
         };
-
         let options_struct = Options::new(true, false);
+
         assert_eq!(config.get_options().is_some(), true);
         assert_eq!(config.get_options().unwrap(), &options_struct);
     }
 
     #[test]
     fn test_config_get_query() {
+        let file = File::open("./test-data/test.txt").unwrap();
         let config = Config {
             options: Some(Options::new(true, false)),
             query: String::from("query"),
-            filename: String::from("filename"),
+            file: file,
         };
 
         assert_eq!(config.get_query(), "query");
     }
 
     #[test]
-    fn test_config_get_filename() {
+    fn test_config_get_file() {
+        let file = File::open("./test-data/test.txt").unwrap();
         let config = Config {
             options: Some(Options::new(true, false)),
             query: String::from("query"),
-            filename: String::from("filename"),
+            file: file.try_clone().unwrap(),
         };
 
-        assert_eq!(config.get_filename(), "filename");
+        assert_eq!(config.get_file().metadata().unwrap().len(), file.metadata().unwrap().len());
+        assert!(config.get_file().metadata().unwrap().is_file());
+        assert_eq!(config.get_file().metadata().unwrap().file_type(), file.metadata().unwrap().file_type());
     }
 }
